1d0457ff23620f8c9516748046d6cc2dff5d48b4
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-07 11:50:45 +0100

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe 'default' do
   context 'with default values for all parameters' do
-    it { should contain_class('testmodule7') }
+    it { should contain_class('default') }
     it { is_expected.to have_resource_count(1) }
   end
 end

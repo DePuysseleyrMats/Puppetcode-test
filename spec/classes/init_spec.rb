e9a8e7af0418524ace067624f399b3e7965cdb67
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-07 15:16:13 +0100

    Update and rename testmodule_spec.rb to init_spec.rb

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+describe 'test' do
+    it { should contain_class('test') }
+  end
--- spec/classes/testmodule_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-describe 'testmodule' do
-    it { should contain_class('testmodule') }
-  end

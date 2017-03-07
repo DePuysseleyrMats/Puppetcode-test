cb3bb9bbe73555c5b77119fa83dd8c12b8778fee
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-07 12:30:16 +0100

    Delete init_spec.rb

--- spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'testmodule7' do
-  context 'with default values for all parameters' do
-    it { should contain_class('testmodule7') }
-    it { is_expected.to have_resource_count(1) }
-  end
-end

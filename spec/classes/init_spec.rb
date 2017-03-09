77e5c716b71411d324181db41403d955386831ea
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-09 10:04:42 +0100

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 describe 'init' do
     it { should contain_class('init') }
+        it { should contain_class('apache') }
     it { is_expected.to compile }
     it { is_expected.to have_resource_count(1) }
     it { is_expected.to contain_package('ntp') }

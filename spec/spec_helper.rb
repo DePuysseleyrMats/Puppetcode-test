f7139914bae7aee59f951f41dc2644ae75153c99
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-09 12:26:07 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet'
 require 'rspec-puppet/spec_helper'
+require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
 
 RSpec.configure do |c|
   c.module_path = '/home/travis/build/DePuysseleyrMats/Puppetcode-test/manifests'
